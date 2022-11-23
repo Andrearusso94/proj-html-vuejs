1f54c412c2e76e96705a8ca5d6035cd6725df278
Andrearusso94 <andrea.r.4845@gmail.com>
2022-11-23 12:55:58 +0100

    add square header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,7 +17,8 @@ export default {
 
             <!--presentazione negozio-->
             <PresentationVue></PresentationVue>
-
+            <div class="square top demos"></div>
+            <div class="square sale"></div>
         </div>
 
 
@@ -31,6 +32,21 @@ export default {
 .container-fluid {
     background-image: url(../assets/img/avadabarbers-homepage-hero-bg.jpg);
     background-size: cover;
+    position: relative;
+
+    .square {
+        height: 50px;
+        width: 50px;
+        position: absolute;
+        top: 100px;
+        left: 97%;
+        background-color: $light-grey;
+        border-radius: 3px;
+    }
+
+    .top {
+        top: 160px;
+    }
 
 }
 </style>
